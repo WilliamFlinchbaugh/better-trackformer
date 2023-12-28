4feda5dfabd9c6f45bd505b3af53442dafcff088
William Flinchbaugh <williamflinchbaugh@gmail.com>
2023-12-27 20:01:26 -0600

    fpn implemented

--- src/trackformer/models/backbone.py
+++ src/trackformer/models/backbone.py
@@ -9,9 +9,8 @@ import torch.nn.functional as F
 import torchvision
 from torch import nn
 from torchvision.models._utils import IntermediateLayerGetter
-from torchvision.ops.feature_pyramid_network import (FeaturePyramidNetwork,
-                                                     LastLevelMaxPool)
-
+from torchvision.ops.feature_pyramid_network import FeaturePyramidNetwork
+from torchvision.models.detection.backbone_utils import LastLevelMaxPool
 from ..util.misc import NestedTensor, is_main_process
 from .position_encoding import build_position_encoding
 
@@ -60,25 +59,44 @@ class BackboneBase(nn.Module):
     def __init__(self, backbone: nn.Module, train_backbone: bool,
                  return_interm_layers: bool):
         super().__init__()
+        self.use_fpn = True
+        
         for name, parameter in backbone.named_parameters():
             if (not train_backbone
                 or 'layer2' not in name
                 and 'layer3' not in name
                 and 'layer4' not in name):
                 parameter.requires_grad_(False)
-        if return_interm_layers:
-            return_layers = {"layer1": "0", "layer2": "1", "layer3": "2", "layer4": "3"}
-            # return_layers = {"layer2": "0", "layer3": "1", "layer4": "2"}
+                
+        if self.use_fpn:
             self.strides = [4, 8, 16, 32]
-            self.num_channels = [256, 512, 1024, 2048]
+            self.channels = [256, 512, 1024, 2048]
+            self.num_channels = [256, 256, 256, 256]
+            
+            self.body = IntermediateLayerGetter(backbone, 
+                return_layers={f'layer{k}': str(v) for v, k in enumerate([1, 2, 3, 4])})
+        
+            self.fpn = FeaturePyramidNetwork(
+                self.channels, out_channels=self.num_channels[0],
+                extra_blocks=LastLevelMaxPool())
+            
         else:
-            return_layers = {'layer4': "0"}
-            self.strides = [32]
-            self.num_channels = [2048]
-        self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
+            if return_interm_layers:
+                return_layers = {"layer1": "0", "layer2": "1", "layer3": "2", "layer4": "3"}
+                # return_layers = {"layer2": "0", "layer3": "1", "layer4": "2"}
+                self.strides = [4, 8, 16, 32]
+                self.num_channels = [256, 512, 1024, 2048]
+            else:
+                return_layers = {'layer4': "0"}
+                self.strides = [32]
+                self.num_channels = [2048]
+            self.body = IntermediateLayerGetter(backbone, return_layers=return_layers)
 
     def forward(self, tensor_list: NestedTensor):
+        print(x.size for x in tensor_list.tensors)
         xs = self.body(tensor_list.tensors)
+        if self.use_fpn:
+            xs = self.top_down(xs)
         out: Dict[str, NestedTensor] = {}
         for name, x in xs.items():
             m = tensor_list.mask
